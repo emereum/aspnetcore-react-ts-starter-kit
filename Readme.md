2309e4f72a6f3e379670c7085196296b8b4fc239
hoflogic <hoflogic@users.noreply.github.com>
2020-04-20 20:24:00 +1000

    Switch to npx as a scaffolding tool.

--- Readme.md
+++ Readme.md
@@ -1,17 +1,10 @@
-# generator-hoflogic-spa
+# ASP.NET Core React TypeScript Starter Kit
 
-This is a Yeoman generator which generates an ASP.NET Core web Api and a React + MobX SPA in TypeScript. Navigate to the [template readme file](generators/app/templates/README.md) to see information about the output of this generator.
+This project generates an ASP.NET Core web Api and a React + MobX SPA in TypeScript. Navigate to the [template readme file](template/README.md) to see information about the output of this generator.
 
 
-# Developing This Script
+# Get Started
 
-## Getting Started
-
-* Open a command prompt at the root of this repository
-* Invoke `npm link` (this can be reverted with `npm unlink --global`)
-* In another directory, invoke `yo hoflogic-spa`
-
-## Altering the Template
-
-* Navigate to `generators\app\templates` to view and edit the template.
-* Navigate to `generators\app\index.js` to see how the template variables are substituted.
+```
+npx aspnetcore-react-ts-starter-kit
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+const prompts = require("prompts");
+const gulp = require("gulp")
+const rename = require("gulp-rename");
+const replace = require("gulp-replace");
+
+(async () => {
+    // Values in the template project which should be substituted with
+    // user-specified values.
+    const substitutions = [
+        {
+            name: "productName",
+            message: "What is the product name (in title case, e.g. ACME Widget)?",            
+            templateValue: "Template Product Name"
+        },
+        {
+            name: "authorName",
+            message: "What is the author name (in title case, e.g. ACME Incorporated)?",
+            templateValue: "Template Author Name"
+        }
+    ];
+
+    const response = await prompts(substitutions.map(x => ({
+        name: x.name,
+        message: x.message,
+        type: "text",
+        validate: x => x.length > 0
+    })));
+
+    const completedSubstitutions = substitutions.map(x => ({...x, value: response[x.name]}));
+        
+    // Describes several transformations of the template values so we can
+    // substitute out different formattings of the template values (such as
+    // PascalCase or kebab-case).
+    const mappers = [
+        // Title Case
+        x => x,
+        // PascalCase
+        x => x.replace(/[^a-zA-Z0-9]/g, ""),
+        // lowercase
+        x => x.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(),
+        // kebab-case
+        x => x.replace(/ /g, "-").replace(/[^-a-zA-Z0-9]/, "").toLowerCase(),
+        // snake_case
+        x => x.replace(/ /g, "_").replace(/[^_a-zA-Z0-9]/, "").toLowerCase()
+    ];
+
+    // Map template file names and content to output
+    mappers
+        .flatMap(mapper => completedSubstitutions.map(sub => ({
+            substitution: sub,
+            mapper: mapper
+        })))
+        .reduce((pipeline, x) => {
+            const { substitution, mapper } = x;
+            const findRegex = new RegExp(mapper(substitution.templateValue), "g");
+            const replaceValue = mapper(substitution.value);
+
+            return pipeline
+                .pipe(rename(path => {
+                    // Map file names
+                    path.basename = path.basename.replace(findRegex, replaceValue);
+                    
+                    // Map directory names
+                    path.dirname = path.dirname.replace(findRegex, replaceValue);
+                }))
+                // Map file contents
+                .pipe(replace(findRegex, replaceValue));
+        },
+        gulp.src("generators/**/*"))
+        .pipe(gulp.dest("output-changeme"));
+})();
